290b376f542f7999229f6dee7b6d5d6f5c15d271
mrrobot911 <kelwin2705@gmail.com>
2023-10-26 21:15:17 +0300

    feat: structure of project

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import '@app/App.tsx';
+import Header from '@features/components/Header';
+import Main from '@features/components/Main';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/features/components/Main.tsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return <p>main part</p>;
+  }
+}
+export default Main;
