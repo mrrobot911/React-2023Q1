dec494f7d913cc570ba32af217e34d1cf57f4f1c
mrrobot911 <kelwin2705@gmail.com>
2023-10-29 19:57:36 +0300

    fix: spinner

--- src/shared/ui/Spinner.tsx
+++ src/shared/ui/Spinner.tsx
@@ -2,19 +2,15 @@ import { PureComponent } from 'react';
 import '@shared/ui/Spinner.css';
 
 export default class Spinner extends PureComponent {
+  array = Array(8).fill(0);
   render() {
+    console.log(this.array);
     return (
       <div className="spinnerBlock">
         <div className="spinner">
-          <div className="sk-cube sk-cube1"></div>
-          <div className="sk-cube sk-cube2"></div>
-          <div className="sk-cube sk-cube3"></div>
-          <div className="sk-cube sk-cube4"></div>
-          <div className="sk-cube sk-cube5"></div>
-          <div className="sk-cube sk-cube6"></div>
-          <div className="sk-cube sk-cube7"></div>
-          <div className="sk-cube sk-cube8"></div>
-          <div className="sk-cube sk-cube9"></div>
+          {this.array.map((_, i) => (
+            <div key={i} className={`sk-cube sk-cube${i + 1}`}></div>
+          ))}
         </div>
         <p>Loading...</p>
       </div>
