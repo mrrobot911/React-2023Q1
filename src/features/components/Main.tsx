d81805243993c8b45b9344b88ba9151dc967d05d
mrrobot911 <kelwin2705@gmail.com>
2023-11-05 13:15:33 +0300

    fix: pagination

--- src/features/components/Main.tsx
+++ src/features/components/Main.tsx
@@ -41,6 +41,7 @@ export default function Main() {
         <Pagination
           totalCount={stateList.totalCount}
           setStateList={setStateList}
+          page={stateList.page}
         />
       </div>
       <Outlet />
--- src/features/components/Pagination.tsx
+++ src/features/components/Pagination.tsx
@@ -6,26 +6,62 @@ import { useSearchParams } from 'react-router-dom';
 interface props {
   totalCount: number;
   setStateList: Dispatch<SetStateAction<State>>;
+  page: number;
 }
-function Pagination({ totalCount, setStateList }: props) {
+function Pagination({ totalCount, setStateList, page }: props) {
   const [searchValue, setSearchValue] = useSearchParams();
-  const numbers = range(0, totalCount / 20);
+  const numbers = range(1, Math.ceil(totalCount / 20));
 
   const changePage = (i: number) => {
     setSearchValue({
       search: searchValue.get('search') || '',
-      page: `${i + 1}`,
+      page: `${i}`,
     });
-    setStateList((prev) => ({ ...prev, page: i + 1 }));
+    setStateList((prev) => ({ ...prev, page: i }));
   };
 
+  const filterList = (i: number) => {
+    switch (i) {
+      case 1:
+      case page - 2:
+      case page - 1:
+      case page:
+      case page + 1:
+      case page + 2:
+      case numbers.length - 1:
+        return true;
+      default:
+        return false;
+    }
+  };
   return (
     <div>
-      {numbers.map((_, i) => (
-        <button type="button" key={i} onClick={() => changePage(i)}>
-          {i + 1}
-        </button>
-      ))}
+      <button
+        type="button"
+        disabled={page === 1}
+        onClick={() => changePage(page - 1)}
+      >
+        left
+      </button>
+      {numbers
+        .filter((i) => filterList(i))
+        .map((i) => (
+          <button
+            type="button"
+            key={i}
+            disabled={i === page}
+            onClick={() => changePage(i)}
+          >
+            {i}
+          </button>
+        ))}
+      <button
+        type="button"
+        disabled={page === numbers.length}
+        onClick={() => changePage(page + 1)}
+      >
+        right
+      </button>
     </div>
   );
 }
